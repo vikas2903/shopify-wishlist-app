b91fae16f48b1c98fadf6f68b54b0b750ca79220
Vikas Prasad <prasadvvikas@gmail.com>
2025-05-21 12:59:11 +0530

    apitoken install update 01 c

--- controller/apitokencontroller.js
+++ controller/apitokencontroller.js
@@ -7,7 +7,7 @@ export const redirectToShopify = async (req, res) => {
   if (!shop) return res.status(400).send("Missing shop parameter");
 
   const redirectUri = "https://shopify-app-7spy.onrender.com/auth/callback";
-  const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${process.env.SHOPIFY_API_KEY}&scope=read_orders,read_products&redirect_uri=${redirectUri}`;
+  const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${process.env.SHOPIFY_API_KEY}&scope=write_products,write_customers,read_products,read_orders,read_discounts&redirect_uri=${redirectUri}`;
 
   res.redirect(installUrl);
 };
