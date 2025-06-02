6051c5fdc058a06160380911d234395721bcb362
Vikas Prasad <prasadvvikas@gmail.com>
2025-06-02 15:43:52 +0530

    Appen liquid file inside app 04

--- route/uploadroute.js
+++ route/uploadroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-import  {uploadBlockSection} from ('../controller/uploadBlockSection.js');
+import  {uploadBlockSection} from '../controller/uploadBlockSection.js';
 
 const uploadroute = express.Router();
 
